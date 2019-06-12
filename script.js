f69c592b8929795a3348b5ba4e3935b79290c791
Emmanuel Tope-Ojo <etopeojo@gmail.com>
2019-06-12 08:56:09 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,100 @@
+/* function MinimumCoinChange(cointypes){
+    this.coins = cointypes;
+    this.numCoinTypes = this.coins.length;
+}
+
+MinimumCoinChange.prototype = {
+
+    generateCoins: function(amount){
+        var finalResult = [];
+        var totalAmount = amount;
+
+        for(var i = 0; i < this.numCoinTypes; i++){
+            var currentTypeValue = this.coins[i];
+            var typeCount = Math.floor(totalAmount / currentTypeValue);
+
+            finalResult.push(typeCount);
+
+            totalAmount -= (currentTypeValue * typeCount);
+        }
+
+        return finalResult;
+    },
+
+    displayResults: function(amount){
+        var results = this.generateCoins(amount);
+        console.log(results);
+        for(var i = 0; i < results.length; i++){
+            console.log(`There are(is) ${results[i]} ${coinTypes[i]}s`);
+        }
+    }
+}
+ */
+//Creating the solution class
+class MinimumCoinChange {
+    constructor(coinTypes) { //Accept the coinType array
+        this.coins = coinTypes;
+        this.numCoinTypes = this.coins.length;
+    }
+
+    //Implementing the coin allocation solution
+    generateCoins(amount) {
+        let finalResult = {};
+        let totalAmount = amount;
+
+        for(let i = 0; i < this.numCoinTypes; i++){
+            let currentTypeValue = this.coins[i];
+            let createCoinValueAsKey = currentTypeValue.toString();
+
+            let typeCount = Math.floor(totalAmount / currentTypeValue);
+
+            finalResult[createCoinValueAsKey] = typeCount;
+
+            totalAmount -= (currentTypeValue * typeCount);
+        }
+
+        return finalResult;
+    }
+
+    //Convert the finalResult to be sorted from highest to lowest, as in a pyramid.
+    convertToPyramidArray(objectToConvert){
+        let sortedResult = [];
+
+        for(let object in objectToConvert){
+            sortedResult.push([object, objectToConvert[object]]);
+        }
+        sortedResult.sort((a, b) => {return b[1] - a[1];});
+        console.log(sortedResult);
+        console.log("Out of Pyramid");
+        return sortedResult;
+    }
+
+    //Display the results
+    displayResults(amount) {
+        let results = this.generateCoins(amount);
+        console.log(results);
+        let sortedResult = this.convertToPyramidArray(results);
+
+        let case2Div = document.querySelector('div.case2');
+        let case2Header =  document.querySelector('h1.case2_header span');
+        case2Header.innerHTML = '#' + amount;
+        
+
+        for(let i = 0; i < sortedResult.length; i++){
+            for(let j = 0; j < 1; j++){
+                let newH3 = document.createElement('p');
+                newH3.textContent = `There are(is) ${sortedResult[i][j+1]} pieces of ${sortedResult[i][j]} naira`;
+                newH3.setAttribute('style', "font-family: 'Lucida Sans', 'Lucida Sans Regular';");
+                case2Div.appendChild(newH3);
+                console.log(`There are(is) ${sortedResult[i][j+1]} pieces of ${sortedResult[i][j]} naira`);
+            }
+        }
+    }
+
+}
+
+var coinTypes = [25,10, 7,2];
+var coinChanger = new MinimumCoinChange(coinTypes);
+
+coinChanger.displayResults(191);
+//console.log(coinChanger.generateCoins(191));
---
+++ script.php
@@ -0,0 +1,46 @@
+<?php
+    //Creating the solution class
+    class MinimumCoinChange{
+        private $coins;
+        private $numCoinType;
+
+
+        function __construct($coinTypes){
+            $this->coins = $coinTypes;
+            $this->numCoinType = count($coinTypes);
+        }
+
+        //Implementing the coin allocation solution
+        function generateCoins($amount) {
+             $finalResult = [];
+             $totalAmount = $amount;
+
+            for($i = 0; $i < $this->numCoinType; $i++){
+                $currentTypeValue = $this->coins[$i];
+
+                $typeCount = floor($totalAmount / $currentTypeValue);
+    
+                $finalResult[strval($this->coins[$i])] = $typeCount;
+    
+                $totalAmount -= ($currentTypeValue * $typeCount);
+            }
+    
+            return $finalResult;
+        }
+
+        function displayResults($amount) {
+            $results = $this->generateCoins($amount);
+            arsort($results);   //Sort the result to be arranged in a pyramid way, that is from highest to the lowest
+
+            echo ("<h1>Coin Generator for amount #$amount ");
+            foreach($results as $key => $value){
+                echo("<h3>There are(is) {$value} pieces of {$key} naira</h3>");
+            }
+        }
+    }
+
+    $cointypes = [25,10,7,2];
+    $coinChanger = new MinimumCoinChange($cointypes);
+
+    $coinChanger->displayResults(191);
+?>
